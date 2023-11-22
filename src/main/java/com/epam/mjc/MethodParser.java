dfb49e34e0afe846777b73b94afde7618a383b70
sergei-zhirnov <s.zhirnov@protonmail.com>
2023-11-22 11:59:39 +0400

    Stage 1, String Task, Attempt 1

--- src/main/java/com/epam/mjc/MethodParser.java
+++ src/main/java/com/epam/mjc/MethodParser.java
@@ -1,25 +1,59 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class MethodParser {
 
-    /**
-     * Parses string that represents a method signature and stores all it's members into a {@link MethodSignature} object.
-     * signatureString is a java-like method signature with following parts:
-     *      1. access modifier - optional, followed by space: ' '
-     *      2. return type - followed by space: ' '
-     *      3. method name
-     *      4. arguments - surrounded with braces: '()' and separated by commas: ','
-     * Each argument consists of argument type and argument name, separated by space: ' '.
-     * Examples:
-     *      accessModifier returnType methodName(argumentType1 argumentName1, argumentType2 argumentName2)
-     *      private void log(String value)
-     *      Vector3 distort(int x, int y, int z, float magnitude)
-     *      public DateTime getCurrentDateTime()
-     *
-     * @param signatureString source string to parse
-     * @return {@link MethodSignature} object filled with parsed values from source string
-     */
-    public MethodSignature parseFunction(String signatureString) {
-        throw new UnsupportedOperationException("You should implement this method.");
+    public static MethodSignature parseFunction(String signatureString) {
+
+        // Getting method arguments
+        int indexOfArgsBeginning = signatureString.indexOf("(");
+        int indexOfArgsEnding = signatureString.indexOf(")");
+        boolean hasArgs = (indexOfArgsBeginning + 1 != indexOfArgsEnding);
+        List<MethodSignature.Argument> argsList = new ArrayList<>();
+
+        if (hasArgs) {
+
+            String argumentsString = signatureString.substring(indexOfArgsBeginning + 1, indexOfArgsEnding);
+            String[] splittedArgumentsString = argumentsString.split(", ");
+            int argsCount = splittedArgumentsString.length;
+            String type;
+            String name;
+            for (int i = 0; i < argsCount; i++) {
+                type = splittedArgumentsString[i].split(" ")[0];
+                name = splittedArgumentsString[i].split(" ")[1];
+                MethodSignature.Argument arg = new MethodSignature.Argument(type, name);
+                argsList.add(i, arg);
+            }
+        }
+
+        // Getting method name, type and access modifier
+        String mainString = signatureString.substring(0, indexOfArgsBeginning);
+        String[] splittedMainString = mainString.split(" ");
+        int lengthOfSplittedMainString = splittedMainString.length;
+
+        String methodName = splittedMainString[lengthOfSplittedMainString-1];
+        String returnType = splittedMainString[lengthOfSplittedMainString-2];
+        String accessModifier = "";
+        if (lengthOfSplittedMainString == 3) {
+            accessModifier = splittedMainString[0];
+        }
+
+
+        // Constructing
+        MethodSignature signatureOutput;
+        if (hasArgs) {
+            signatureOutput = new MethodSignature(methodName, argsList);
+        } else {
+            signatureOutput = new MethodSignature(methodName);
+        }
+        signatureOutput.setReturnType(returnType);
+
+        if (!Objects.equals(accessModifier, "")) {
+            signatureOutput.setAccessModifier(accessModifier);
+        }
+        return signatureOutput;
     }
 }
--- src/main/java/com/epam/mjc/StringSplitter.java
+++ src/main/java/com/epam/mjc/StringSplitter.java
@@ -1,18 +1,54 @@
 package com.epam.mjc;
 
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
+import java.lang.Math;
 
 public class StringSplitter {
 
-    /**
-     * Splits given string applying all delimeters to it. Keeps order of result substrings as in source string.
-     *
-     * @param source source string
-     * @param delimiters collection of delimiter strings
-     * @return List of substrings
-     */
+    public static String getRandomString(int len) {
+        {
+            String AlphaNumericStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvxyz0123456789АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+            StringBuilder s = new StringBuilder(len);
+            for (int i = 0; i < len; i++) {
+                int ch = (int)(AlphaNumericStr.length() * Math.random());
+                s.append(AlphaNumericStr.charAt(ch));
+            }
+            return s.toString();
+        }
+    }
+
+
+
     public List<String> splitByDelimiters(String source, Collection<String> delimiters) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        List<String> delimetersList = new ArrayList<>(delimiters);
+        String randomDelimeter = "";
+        for (Object o: delimetersList) {
+            do {
+                randomDelimeter = getRandomString(1);
+            } while (source.contains(randomDelimeter) || o.toString().contains(randomDelimeter));
+        }
+
+
+        for (Object o : delimetersList) {
+            String del = o.toString();
+            source = source.replace(del, randomDelimeter);
+        }
+
+        String[] preresult = source.split(randomDelimeter);
+        ArrayList<String> arrPreresult = new ArrayList<>(List.of(preresult));
+        ArrayList<String> arrResult = new ArrayList<>();
+        Iterator<String> iterator = arrPreresult.iterator();
+
+        while (iterator.hasNext()) {
+            Object element = iterator.next();
+            if (Objects.equals(element.toString(), "")) {
+                iterator.remove();
+            } else {
+                arrResult.add(element.toString());
+            }
+        }
+
+
+        return arrResult;
     }
 }
